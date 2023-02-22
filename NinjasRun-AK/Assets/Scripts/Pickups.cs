d4d44442162fead8ba3333605db91c6bae8db5d6
BillySwCodeNinjas <andrewkaiser2006@gmail.com>
2023-02-21 19:00:42 -0600

    on step 8b

---
+++ NinjasRun-AK/Assets/Scripts/Pickups.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickups : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+
+    public ParticleSystem Pickup;
+
+// will activate when object enter a collision zone 
+// aka entering a box colider
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("coin")){
+            score++;
+            scoreText.text = score.ToString();
+            Destroy(other.gameObject);
+            // can use tags to spesify the collision, 
+            // if multiple things have collisions
+        }
+        Debug.Log("Trigger Enter");
+    }
+    void Start()
+    {
+       
+    }
+
+    
+}
